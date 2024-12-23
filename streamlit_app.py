32e5d0e2f797ef74baa509789d6e1deafacdcdf8
Preksha-Pandya <preksha.pandya@inferenz.ai>
2024-12-23 15:38:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ name_on_order = st.text_input('Name on smoothie')
 st.write("The name on your smoothie will be : ", name_on_order)
 
 
-my_dataframe=session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'))
+my_dataframe=session.table('smoothies.public.fruit_options').select(col('search_on'))
 st.dataframe(data=my_dataframe,use_container_width=True)
 st.stop()
 
