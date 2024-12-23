eff88291a6f0bc6b8c617bec5e2b99514c7ae8f6
Preksha-Pandya <preksha.pandya@inferenz.ai>
2024-12-23 15:16:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,7 @@ if ingredient_list:
     
     for fruit in ingredient_list:
         ingredients_string=ingredients_string+fruit+' '
+        st.subheader(fruit+'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
